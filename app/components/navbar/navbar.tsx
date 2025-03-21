81de72449e2c724f9d7b831c2d0a9fc8542fbe40
jonathanyangg <jonathan.yang.x@gmail.com>
2025-03-20 23:20:37 -0400

    Add Navbar component and integrate into layout

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 w-full bg-white z-10 shadow-sm">
+      Navbar!
+    </div>
+  )
+}
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import {Nunito} from "next/font/google"
 import "./globals.css";
+import Navbar from "./components/navbar/navbar";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -18,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <div className="h-[2000px] margin-top-[100px]">
+          <Navbar />
+          {children}
+        </div>
+        
+      </body>
     </html>
   );
 }
